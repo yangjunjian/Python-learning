7a36c21f1154539b95a0b2e2403ea3c3a416078b
junjian yang <junjian.yang@yahoo.com>
2021-01-06 16:38:39 +0800

    Create use_slots.py

---
+++ oop_advance/use_slots.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""a test script of class/instance"""
+__author__ = 'Jason Yang'
+
+
+class Student(object):
+    __slots__ = ('name', 'age')
+
+
+class GraduateStudent(Student):
+    pass
+
+
+s = Student()
+s.name = 'Jason'
+s.age = 25
+
+try:
+    s.score = 99
+except AttributeError as e:
+    print('AttributeError:', e)
+
+g = GraduateStudent()
+g.score = 100
+print('g.score =',g.score)
