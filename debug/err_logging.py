b2c8f8829c8b49691db4e710174f6062495c7ad1
junjian yang <junjian.yang@yahoo.com>
2021-01-27 16:36:15 +0800

    Create err_logging.py

---
+++ debug/err_logging.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""a test script of error logging"""
+__author__ = 'Jason Yang'
+
+import logging
+
+
+def foo(s):
+    return 10 / int(s)
+
+
+def bar(s):
+    return foo(s) * 2
+
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+
+
+main()
+print('END')
