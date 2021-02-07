f0f83900a53b258ef3ebf04580ec9e91658a1e63
junjian yang <junjian.yang@yahoo.com>
2021-02-07 14:32:05 +0800

    Create with_file.py

---
+++ io/with_file.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""a test script of with file"""
+__author__ = 'Jason Yang'
+
+from datetime import datetime
+
+with open('test.txt', 'w') as f:
+    f.write('今天是：')
+    f.write(datetime.now().strftime('%Y-%m-%d'))
+
+with open('test.txt', 'r') as f:
+    s = f.read()
+    print('open for read...')
+    print(s)
+
+with open('test.txt', 'rb') as f:
+    b = f.read()
+    print('open as binary for read...')
+    print(b)
