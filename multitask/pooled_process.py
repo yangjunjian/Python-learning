a0be06997e1ea6fe63046a2702b4035178805dac
junjian yang <junjian.yang@yahoo.com>
2021-02-23 15:54:47 +0800

    Update pooled_process.py

--- multitask/pooled_process.py
+++ multitask/pooled_process.py
@@ -4,26 +4,26 @@
 """a test script of multiprocessing"""
 __author__ = 'Jason Yang'
 
-import os
-import time
-import random
+import os, time, random
 from multiprocessing import Pool
 
 
-def long_time_task(name):
+def long_time_proc(name):
     print('Run task %s (%s)...' % (name, os.getpid()))
     start = time.time()
     time.sleep(random.random() * 3)
     end = time.time()
-    print('Task %s runs in %.2f seconds.' % (name, (end - start)))
+    print('Task %s runs in %.2f seconds.' % (name, os.getpid()))
 
 
 if __name__ == '__main__':
     print('Parent process %s.' % os.getpid())
     p = Pool(4)
     for i in range(5):
-        p.apply_async(long_time_task, args=(i, ))
-    print('Waiting for all subprocesses done...')
+        p.apply_async(long_time_proc, args=(i, ))
+    print('Waiting for all subprocesses done.')
+    #    关闭进程池，不再接受新任务
     p.close()
+    #    主进程阻塞，等待子进程退出
     p.join()
     print('All subprocesses done.')
